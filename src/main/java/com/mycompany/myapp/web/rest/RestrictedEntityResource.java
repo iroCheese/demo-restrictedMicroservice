78f9fb65b0bad8d485edaa3e2e6d18e957ed357c
Samuel Brendler <samuel@localhost.localdomain>
2019-07-12 14:02:16 +0200

    Initial application generated by JHipster-6.1.0

---
+++ src/main/java/com/mycompany/myapp/web/rest/RestrictedEntityResource.java
@@ -0,0 +1,117 @@
+package com.mycompany.myapp.web.rest;
+
+import com.mycompany.myapp.domain.RestrictedEntity;
+import com.mycompany.myapp.repository.RestrictedEntityRepository;
+import com.mycompany.myapp.web.rest.errors.BadRequestAlertException;
+
+import io.github.jhipster.web.util.HeaderUtil;
+import io.github.jhipster.web.util.ResponseUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * REST controller for managing {@link com.mycompany.myapp.domain.RestrictedEntity}.
+ */
+@RestController
+@RequestMapping("/api")
+public class RestrictedEntityResource {
+
+    private final Logger log = LoggerFactory.getLogger(RestrictedEntityResource.class);
+
+    private static final String ENTITY_NAME = "restrictedMicroserviceRestrictedEntity";
+
+    @Value("${jhipster.clientApp.name}")
+    private String applicationName;
+
+    private final RestrictedEntityRepository restrictedEntityRepository;
+
+    public RestrictedEntityResource(RestrictedEntityRepository restrictedEntityRepository) {
+        this.restrictedEntityRepository = restrictedEntityRepository;
+    }
+
+    /**
+     * {@code POST  /restricted-entities} : Create a new restrictedEntity.
+     *
+     * @param restrictedEntity the restrictedEntity to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new restrictedEntity, or with status {@code 400 (Bad Request)} if the restrictedEntity has already an ID.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PostMapping("/restricted-entities")
+    public ResponseEntity<RestrictedEntity> createRestrictedEntity(@RequestBody RestrictedEntity restrictedEntity) throws URISyntaxException {
+        log.debug("REST request to save RestrictedEntity : {}", restrictedEntity);
+        if (restrictedEntity.getId() != null) {
+            throw new BadRequestAlertException("A new restrictedEntity cannot already have an ID", ENTITY_NAME, "idexists");
+        }
+        RestrictedEntity result = restrictedEntityRepository.save(restrictedEntity);
+        return ResponseEntity.created(new URI("/api/restricted-entities/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * {@code PUT  /restricted-entities} : Updates an existing restrictedEntity.
+     *
+     * @param restrictedEntity the restrictedEntity to update.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated restrictedEntity,
+     * or with status {@code 400 (Bad Request)} if the restrictedEntity is not valid,
+     * or with status {@code 500 (Internal Server Error)} if the restrictedEntity couldn't be updated.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PutMapping("/restricted-entities")
+    public ResponseEntity<RestrictedEntity> updateRestrictedEntity(@RequestBody RestrictedEntity restrictedEntity) throws URISyntaxException {
+        log.debug("REST request to update RestrictedEntity : {}", restrictedEntity);
+        if (restrictedEntity.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        RestrictedEntity result = restrictedEntityRepository.save(restrictedEntity);
+        return ResponseEntity.ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, restrictedEntity.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * {@code GET  /restricted-entities} : get all the restrictedEntities.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of restrictedEntities in body.
+     */
+    @GetMapping("/restricted-entities")
+    public List<RestrictedEntity> getAllRestrictedEntities() {
+        log.debug("REST request to get all RestrictedEntities");
+        return restrictedEntityRepository.findAll();
+    }
+
+    /**
+     * {@code GET  /restricted-entities/:id} : get the "id" restrictedEntity.
+     *
+     * @param id the id of the restrictedEntity to retrieve.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the restrictedEntity, or with status {@code 404 (Not Found)}.
+     */
+    @GetMapping("/restricted-entities/{id}")
+    public ResponseEntity<RestrictedEntity> getRestrictedEntity(@PathVariable Long id) {
+        log.debug("REST request to get RestrictedEntity : {}", id);
+        Optional<RestrictedEntity> restrictedEntity = restrictedEntityRepository.findById(id);
+        return ResponseUtil.wrapOrNotFound(restrictedEntity);
+    }
+
+    /**
+     * {@code DELETE  /restricted-entities/:id} : delete the "id" restrictedEntity.
+     *
+     * @param id the id of the restrictedEntity to delete.
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     */
+    @DeleteMapping("/restricted-entities/{id}")
+    public ResponseEntity<Void> deleteRestrictedEntity(@PathVariable Long id) {
+        log.debug("REST request to delete RestrictedEntity : {}", id);
+        restrictedEntityRepository.deleteById(id);
+        return ResponseEntity.noContent().headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString())).build();
+    }
+}
---
+++ src/test/java/com/mycompany/myapp/web/rest/RestrictedEntityResourceIT.java
@@ -0,0 +1,252 @@
+package com.mycompany.myapp.web.rest;
+
+import com.mycompany.myapp.RestrictedMicroserviceApp;
+import com.mycompany.myapp.domain.RestrictedEntity;
+import com.mycompany.myapp.repository.RestrictedEntityRepository;
+import com.mycompany.myapp.web.rest.errors.ExceptionTranslator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.Validator;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.mycompany.myapp.web.rest.TestUtil.createFormattingConversionService;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Integration tests for the {@Link RestrictedEntityResource} REST controller.
+ */
+@SpringBootTest(classes = RestrictedMicroserviceApp.class)
+public class RestrictedEntityResourceIT {
+
+    private static final String DEFAULT_NAME = "AAAAAAAAAA";
+    private static final String UPDATED_NAME = "BBBBBBBBBB";
+
+    @Autowired
+    private RestrictedEntityRepository restrictedEntityRepository;
+
+    @Autowired
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Autowired
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+    @Autowired
+    private ExceptionTranslator exceptionTranslator;
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private Validator validator;
+
+    private MockMvc restRestrictedEntityMockMvc;
+
+    private RestrictedEntity restrictedEntity;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        final RestrictedEntityResource restrictedEntityResource = new RestrictedEntityResource(restrictedEntityRepository);
+        this.restRestrictedEntityMockMvc = MockMvcBuilders.standaloneSetup(restrictedEntityResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setControllerAdvice(exceptionTranslator)
+            .setConversionService(createFormattingConversionService())
+            .setMessageConverters(jacksonMessageConverter)
+            .setValidator(validator).build();
+    }
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static RestrictedEntity createEntity(EntityManager em) {
+        RestrictedEntity restrictedEntity = new RestrictedEntity()
+            .name(DEFAULT_NAME);
+        return restrictedEntity;
+    }
+    /**
+     * Create an updated entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static RestrictedEntity createUpdatedEntity(EntityManager em) {
+        RestrictedEntity restrictedEntity = new RestrictedEntity()
+            .name(UPDATED_NAME);
+        return restrictedEntity;
+    }
+
+    @BeforeEach
+    public void initTest() {
+        restrictedEntity = createEntity(em);
+    }
+
+    @Test
+    @Transactional
+    public void createRestrictedEntity() throws Exception {
+        int databaseSizeBeforeCreate = restrictedEntityRepository.findAll().size();
+
+        // Create the RestrictedEntity
+        restRestrictedEntityMockMvc.perform(post("/api/restricted-entities")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(restrictedEntity)))
+            .andExpect(status().isCreated());
+
+        // Validate the RestrictedEntity in the database
+        List<RestrictedEntity> restrictedEntityList = restrictedEntityRepository.findAll();
+        assertThat(restrictedEntityList).hasSize(databaseSizeBeforeCreate + 1);
+        RestrictedEntity testRestrictedEntity = restrictedEntityList.get(restrictedEntityList.size() - 1);
+        assertThat(testRestrictedEntity.getName()).isEqualTo(DEFAULT_NAME);
+    }
+
+    @Test
+    @Transactional
+    public void createRestrictedEntityWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = restrictedEntityRepository.findAll().size();
+
+        // Create the RestrictedEntity with an existing ID
+        restrictedEntity.setId(1L);
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restRestrictedEntityMockMvc.perform(post("/api/restricted-entities")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(restrictedEntity)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the RestrictedEntity in the database
+        List<RestrictedEntity> restrictedEntityList = restrictedEntityRepository.findAll();
+        assertThat(restrictedEntityList).hasSize(databaseSizeBeforeCreate);
+    }
+
+
+    @Test
+    @Transactional
+    public void getAllRestrictedEntities() throws Exception {
+        // Initialize the database
+        restrictedEntityRepository.saveAndFlush(restrictedEntity);
+
+        // Get all the restrictedEntityList
+        restRestrictedEntityMockMvc.perform(get("/api/restricted-entities?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(restrictedEntity.getId().intValue())))
+            .andExpect(jsonPath("$.[*].name").value(hasItem(DEFAULT_NAME.toString())));
+    }
+    
+    @Test
+    @Transactional
+    public void getRestrictedEntity() throws Exception {
+        // Initialize the database
+        restrictedEntityRepository.saveAndFlush(restrictedEntity);
+
+        // Get the restrictedEntity
+        restRestrictedEntityMockMvc.perform(get("/api/restricted-entities/{id}", restrictedEntity.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.id").value(restrictedEntity.getId().intValue()))
+            .andExpect(jsonPath("$.name").value(DEFAULT_NAME.toString()));
+    }
+
+    @Test
+    @Transactional
+    public void getNonExistingRestrictedEntity() throws Exception {
+        // Get the restrictedEntity
+        restRestrictedEntityMockMvc.perform(get("/api/restricted-entities/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updateRestrictedEntity() throws Exception {
+        // Initialize the database
+        restrictedEntityRepository.saveAndFlush(restrictedEntity);
+
+        int databaseSizeBeforeUpdate = restrictedEntityRepository.findAll().size();
+
+        // Update the restrictedEntity
+        RestrictedEntity updatedRestrictedEntity = restrictedEntityRepository.findById(restrictedEntity.getId()).get();
+        // Disconnect from session so that the updates on updatedRestrictedEntity are not directly saved in db
+        em.detach(updatedRestrictedEntity);
+        updatedRestrictedEntity
+            .name(UPDATED_NAME);
+
+        restRestrictedEntityMockMvc.perform(put("/api/restricted-entities")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(updatedRestrictedEntity)))
+            .andExpect(status().isOk());
+
+        // Validate the RestrictedEntity in the database
+        List<RestrictedEntity> restrictedEntityList = restrictedEntityRepository.findAll();
+        assertThat(restrictedEntityList).hasSize(databaseSizeBeforeUpdate);
+        RestrictedEntity testRestrictedEntity = restrictedEntityList.get(restrictedEntityList.size() - 1);
+        assertThat(testRestrictedEntity.getName()).isEqualTo(UPDATED_NAME);
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistingRestrictedEntity() throws Exception {
+        int databaseSizeBeforeUpdate = restrictedEntityRepository.findAll().size();
+
+        // Create the RestrictedEntity
+
+        // If the entity doesn't have an ID, it will throw BadRequestAlertException
+        restRestrictedEntityMockMvc.perform(put("/api/restricted-entities")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(restrictedEntity)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the RestrictedEntity in the database
+        List<RestrictedEntity> restrictedEntityList = restrictedEntityRepository.findAll();
+        assertThat(restrictedEntityList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    public void deleteRestrictedEntity() throws Exception {
+        // Initialize the database
+        restrictedEntityRepository.saveAndFlush(restrictedEntity);
+
+        int databaseSizeBeforeDelete = restrictedEntityRepository.findAll().size();
+
+        // Delete the restrictedEntity
+        restRestrictedEntityMockMvc.perform(delete("/api/restricted-entities/{id}", restrictedEntity.getId())
+            .accept(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isNoContent());
+
+        // Validate the database is empty
+        List<RestrictedEntity> restrictedEntityList = restrictedEntityRepository.findAll();
+        assertThat(restrictedEntityList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+
+    @Test
+    @Transactional
+    public void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(RestrictedEntity.class);
+        RestrictedEntity restrictedEntity1 = new RestrictedEntity();
+        restrictedEntity1.setId(1L);
+        RestrictedEntity restrictedEntity2 = new RestrictedEntity();
+        restrictedEntity2.setId(restrictedEntity1.getId());
+        assertThat(restrictedEntity1).isEqualTo(restrictedEntity2);
+        restrictedEntity2.setId(2L);
+        assertThat(restrictedEntity1).isNotEqualTo(restrictedEntity2);
+        restrictedEntity1.setId(null);
+        assertThat(restrictedEntity1).isNotEqualTo(restrictedEntity2);
+    }
+}
